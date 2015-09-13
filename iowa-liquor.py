1f3e37c3d20a674653476704f3b734234c5138b5
lzhan94 <lz2371@columbia.edu>
2015-09-13 12:55:43 -0400

    Update iowa-liquor.py

--- iowa-liquor.py
+++ iowa-liquor.py
@@ -2,7 +2,6 @@ import csv
 
 f = open('iowa-liquor-sample.csv','r')
 counter = 0
-#with open('iowa-liquor-sample.csv',rb) as file:
 for line in f:
 	if line.lower().find('single malt scotch')>=0:
 		counter += 1
